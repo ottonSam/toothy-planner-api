9d46ee29cd86a9d5be92b6bc221d11feac4f9e5b
Samuel Otton <ottonsam@gmail.com>
2026-05-13 16:08:56 -0300

    initial commit

--- src/test/java/br/com/ottonsam/toothy_planner_api/ToothyPlannerApiApplicationTests.java
+++ src/test/java/br/com/ottonsam/toothy_planner_api/ToothyPlannerApiApplicationTests.java
@@ -2,12 +2,12 @@ package br.com.ottonsam.toothy_planner_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ToothyPlannerApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
